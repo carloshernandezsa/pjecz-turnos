648e357ff12ca2b40b19168fbd6486e9bea2e0d6
Carlos Hernandez <carlos_hdz57@hotmail.com>
2021-12-06 09:35:29 -0600

    requerimientos

--- models.py
+++ models.py
@@ -13,7 +13,17 @@ class Turno(db.Model):
     ventanilla_id = db.Column(db.Integer)
     estado = db.Column(db.Integer)
     comentarios = db.Column(db.String(250))
-    
+
+class Ventanilla(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    autoridad_id = db.Column(db.Integer)
+    numero = db.Column(db.Integer)
+    descripcion = db.Column(db.String(50))
+    creado = db.Column(db.DateTime , nullable=True)
+    usuario_id = db.Column(db.Integer)
+    estatus = db.Column(db.Integer)
+
+        
 class Usuarios(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombres = db.Column(db.String(45))
@@ -24,15 +34,6 @@ class Usuarios(db.Model):
     rol_id = db.Column(db.Integer)
     autoridad_id = db.Column(db.Integer)
     
-
-class Ventanilla(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    autoridad_id = db.Column(db.Integer)
-    numero = db.Column(db.Integer)
-    descripcion = db.Column(db.String(50))
-    creado = db.Column(db.DateTime , nullable=True)
-    usuario_id = db.Column(db.Integer)
-    estatus = db.Column(db.Integer)
     
 class Autoridades(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -46,6 +47,7 @@ class Departamentos(db.Model):
     nombre = db.Column(db.String(100))
     nombre_corto = db.Column(db.String(50))    
     
-class Roles(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(30))
+#class Roles(db.Model):
+#    id = db.Column(db.Integer, primary_key=True)
+#    nombre = db.Column(db.String(30))
+ 
